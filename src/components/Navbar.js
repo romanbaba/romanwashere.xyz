705e65acbb28510f3a70efd3e5930c15f4f83f56
Roman <108236042+romanwashere@users.noreply.github.com>
2023-03-05 20:10:37 +0300

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {   
+    return (
+       <div>
+        <div className='h-2 bg-violet-500'>
+           
+        </div>
+       </div>
+    )
+}
