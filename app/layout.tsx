3ca30c626a79bf014730d02179a48563c8cd08be
Roman <108236042+romanbaba@users.noreply.github.com>
2024-01-24 15:40:12 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Ewgsta',
-  description: ' Hi, I’m @ewgsta I\'m currently learning I am currently living in Adana, Turkey.',
+  title: 'Roman',
+  description: ' Hi, I’m @romanbaba I\'m currently learning I am currently living in Adana, Turkey.',
   themeColor: '#8B5CF6',
   robots: 'index, follow',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
   icons: 'https://cdn.discordapp.com/emojis/753954444902924339.png',
   openGraph: {
-    images: 'https://cdn.discordapp.com/emojis/753954444902924339.png',
+    image: 'https://cdn.discordapp.com/emojis/753954444902924339.png',
   },
 };
 
